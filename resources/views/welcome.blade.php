80fd764025de49af5de9b4fc4fcd0e1ae45b3c20
Cristian Almanzar <cristian.almanzar@gmail.com>
2020-08-09 17:03:15 -0400

    HTTPS for jquery and web-widget

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,8 +25,15 @@
             };
         </script>
 
-        <script src="{{asset('js/jquery.min.js')}}"></script>
-        <script src="{{asset('js/widget.js')}}"></script>
+        <!-- <script src="{{asset('js/jquery.min.js')}}"></script> -->
+        <!-- <script src="{{asset('js/widget.js')}}"></script> -->
+        <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous">
+        </script>
+
+        <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
         <script src="{{'js/chatbot.js'}}"></script>
     </body>
 </html>
